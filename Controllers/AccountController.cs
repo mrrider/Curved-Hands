7151c270090b2c3d1952606569f6592d2163ef73
mrrider <cozak.nazar@gmail.com>
2017-04-13 19:30:02 +0000

    some

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -191,24 +191,7 @@ namespace TTP_Project.Controllers
             return View();
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Disassociate(string loginProvider, string providerKey)
-        {
-            ManageMessageId? message = null;
-            IdentityResult result = await UserManager.RemoveLoginAsync(User.Identity.GetUserId(), new UserLoginInfo(loginProvider, providerKey));
-            if (result.Succeeded)
-            {
-                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
-                await SignInAsync(user, isPersistent: false);
-                message = ManageMessageId.RemoveLoginSuccess;
-            }
-            else
-            {
-                message = ManageMessageId.Error;
-            }
-            return RedirectToAction("Manage", new { Message = message });
-        }
+        
         
         public ActionResult Manage(ManageMessageId? message)
         {
